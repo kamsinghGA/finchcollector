86833a0f94f6093821363383d1acc24bea83ef6e
kamsinghGA <kamaljitga@gmail.com>
2023-07-12 15:43:57 -0400

    updated assignment

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    age = models.IntegerField()
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+
+    def get_absolute_url(self):
+        return reverse('finches_index')
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
+from .models import Finch
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
-finches = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
-
 def home(request):
     return render(request, 'home.html')
 
@@ -13,6 +11,31 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches':finches
     })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/details.html', {
+        'finch' : finch
+    })
+
+# class FinchCreate(CreateView):
+#     model = Finch
+#     fields = ['name', 'color', 'age', 'description']
+#     template_name = 'main_app/finch_form.html'
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = ['name', 'color', 'age', 'description']
+    template_name = 'main_app/finch_form.html'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['name', 'color', 'age', 'description']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches' 
